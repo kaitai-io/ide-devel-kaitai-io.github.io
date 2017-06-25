35f932d031aa0e7a6d6cdb588b410caa1ea266b9
KT <koczkatamas@gmail.com>
2017-06-25 19:53:56 +0200

    This repo is not used anymore (redir to kaitai.io/devel)

--- js/config.js
+++
@@ -1,2 +0,0 @@
-var kaitaiIde = kaitaiIde || {};
-kaitaiIde.version = '0.1.0.868';
--- js/entities.js
+++
@@ -1,7 +0,0 @@
-class ObjectType {
-}
-ObjectType.Primitive = "Primitive";
-ObjectType.Array = "Array";
-ObjectType.TypedArray = "TypedArray";
-ObjectType.Object = "Object";
-ObjectType.Undefined = "Undefined";
